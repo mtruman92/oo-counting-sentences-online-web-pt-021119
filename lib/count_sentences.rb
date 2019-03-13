bc33bb54eed7c4fb8d89b3f83b28d90619a30d74
Mickae Truman <mtruman@nyit.edu>
2019-03-13 01:15:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".") ? true:false
     
   end
 
   def question?
+    self.end_with?("?") ? true: false
 
   end
 
   def exclamation?
+    self.end_with?("!") ? true:false
 
   end
 
   def count_sentences
-
+    sentence = self.split(/[.?!]/)
+    sentence.delete_if{|sentence| sentence.empty?}
+    sentence.length
   end
 end
